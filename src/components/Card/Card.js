0cff607c1017ee62e6face253fba21e18c3948df
aaron <jaarongreen@gmail.com>
2020-09-28 13:29:16 -0500

    Move to a two-column card layout, add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,39 @@
+// @flow strict
+import React from 'react';
+import moment from 'moment';
+import { Link } from 'gatsby';
+import Img from "gatsby-image";
+import type { Node } from '../../types';
+
+type Props = {
+  content: Node
+};
+
+const Card = ({ content }: Props) => (
+<div className="max-w-sm rounded overflow-hidden shadow-lg my-2 bg-pine border border-olivine" key={content.fields.slug}>
+    <Link to={content.fields.slug}><img className="w-full" src={content.frontmatter.featuredImage.src} alt={content.frontmatter.featuredImage.alt} /></Link>
+    <div className="px-4 py-4">
+        <h2 className="text-2xl my-2">
+            <Link className="text-white" to={content.fields.slug}>{content.frontmatter.title}</Link>
+        </h2>
+        <p className="my-4">{content.frontmatter.description}</p>
+    </div>
+    <div className="px-6 py-4">
+    <p>
+        <time className="text-sm text-gray-400 font-light uppercase py-4" dateTime={moment(content.frontmatter.date).format('MMMM D, YYYY')}>
+            {moment(content.frontmatter.date).format('MMMM YYYY')}
+        </time>
+        <span className="text-sm font-semibold uppercase pl-4 text-primary">
+            <Link to={content.fields.categorySlug} className="">{content.frontmatter.category}</Link>
+        </span>
+    </p>
+    </div>
+    <div className="px-6 py-4">
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+    </div>
+</div>
+);
+
+export default Card;
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,9 +1,7 @@
 // @flow strict
 import React from 'react';
-import moment from 'moment';
-import { Link } from 'gatsby';
-import Img from "gatsby-image";
 import type { Edges } from '../../types';
+import Card from "../Card";
 
 type Props = {
   edges: Edges
@@ -11,30 +9,11 @@ type Props = {
 
 const Feed = ({ edges }: Props) => (
   <div className="container mx-auto p-6 max-w-screen-lg">
-    {edges.map((edge) => (
-      <div class="mb-12 border-white border border-solid" key={edge.node.fields.slug}>
-        <div className="flex flex-col-reverse md:flex-row justify-center flex-wrap">
-          <div className="w-full md:w-3/5 p-6">
-            <p>
-              <time className="text-sm text-gray-400 font-light uppercase py-4" dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-                  {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
-              </time>
-              <span className="text-sm font-semibold uppercase pl-4 text-primary">
-                  <Link to={edge.node.fields.categorySlug} className="">{edge.node.frontmatter.category}</Link>
-              </span>
-            </p>
-            <h2 className="text-3xl my-2">
-                <Link className="text-white" to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
-            </h2>
-            <p className="my-4">{edge.node.frontmatter.description}</p>
-            <p className="mt-10"><Link className="border border-white text-lg rounded-lg p-3" to={edge.node.fields.slug}>Read More</Link></p>
-          </div>
-          <div className="w-full md:w-2/5">
-            <Link to={edge.node.fields.slug}><img src={edge.node.frontmatter.featuredImage.src} /></Link>
-          </div>
-        </div>
-      </div>
-    ))}
+    <div class="grid grid-cols-2 gap-2">
+      {edges.map((edge) => (
+        <Card content={edge.node} />
+      ))}
+    </div>
   </div>
 );
 
